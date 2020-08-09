b64b5e24fde1aa6d6854f7f4d501f813b5266d3f
iamangelu <67723309+iamangelu@users.noreply.github.com>
2020-08-09 21:14:02 +0800

    Updates

---
+++ precache-manifest.afa87a2d3a4b8235ac628c7601572213.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "635bdd24fbb9d02628f13504d501df04",
+    "url": "/instagram-clone/index.html"
+  },
+  {
+    "revision": "3685aa1705ca0cf94ce9",
+    "url": "/instagram-clone/static/css/main.5530058d.chunk.css"
+  },
+  {
+    "revision": "fbdcffbeac8eef735405",
+    "url": "/instagram-clone/static/js/2.ed38edbc.chunk.js"
+  },
+  {
+    "revision": "5f8be6edcb631ea3c834b8459dc2c264",
+    "url": "/instagram-clone/static/js/2.ed38edbc.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "3685aa1705ca0cf94ce9",
+    "url": "/instagram-clone/static/js/main.baf82c31.chunk.js"
+  },
+  {
+    "revision": "9d73230a1dae251b7590",
+    "url": "/instagram-clone/static/js/runtime-main.8aef135a.js"
+  }
+]);
